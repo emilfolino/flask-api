de61adba7e05214cc622838937a5adfc09a1e5f3
Emil Folino <emilfolino@gmail.com>
2021-02-18 12:23:44 +0100

    Before static and css

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+import json
+
 from flask import jsonify
+
 import requests
 
 @app.route('/')
@@ -18,27 +21,68 @@ def root():
     return jsonify(data)
 
 
+def filter_unique(products):
+    unique_dict = {}
+    for key in products["data"]:
+        if not isinstance(key["stock"], int):
+            key["stock"] = 0
 
-@app.route('/unique')
-def unique():
+        if key["name"] in unique_dict:
+            unique_dict[key["name"]] += key["stock"]
+        else:
+            unique_dict[key["name"]] = key["stock"]
+
+    return unique_dict
+
+def create_list(unique_dict):
+    unique_list = []
+    for name in unique_dict:
+        element = {
+            "name": name,
+            "stock": unique_dict[name]
+        }
+        unique_list.append(element)
+
+    return unique_list
+
+
+def fetch_data():
     URL = 'https://lager.emilfolino.se/v2/products/everything'
 
     # Use requests module to get JSON data
     response = requests.get(URL)
 
     # Turns response json object into a Dictionary
-    products_dict = response.json()
+    return response.json()
 
-    return jsonify(products_dict)
+
+@app.route('/unique')
+def unique():
+    products_dict = fetch_data()
+
+    unique_dict = filter_unique(products_dict)
+
+    json_output = { "data" : create_list(unique_dict) }
+
+    return jsonify(json_output)
 
 
 
 @app.route('/search/<query>')
-def show_user_profile(query):
-    # show the user profile for that user
-    data = { "data": query }
+def search(query):
+    products_dict = fetch_data()
 
-    return jsonify(data)
+    unique_dict = filter_unique(products_dict)
+
+    search_results = {}
+    for name in unique_dict:
+        if query in name:
+            search_results[name] = unique_dict[name]
+
+    json_output = { "data" : create_list(search_results) }
+
+    return jsonify(json_output)
+
 
 
 @app.route('/image_search')
